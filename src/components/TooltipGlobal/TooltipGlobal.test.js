ce857f53425078413f8c8baddfca0a659b531ce7
Jas Vadgama <jasal.vadgama@travelperk.com>
2025-01-31 16:24:03 +0000

    fix: update userEvent implmentation

--- src/components/TooltipGlobal/TooltipGlobal.test.js
+++ src/components/TooltipGlobal/TooltipGlobal.test.js
@@ -1,19 +1,19 @@
-import { queryHelpers, render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { MockJetsContextProvider } from '../../__mocks__/MockJetsContext';
 
 import { JetsTooltipGlobal } from './index';
-
 import { activeTooltipData, paramsData } from './__fixtures__';
-import { userEvent } from '@storybook/testing-library';
 
-const setup = ({ data = {}, config = {}, params = {}, events = {} } = {}) => {
-  render(
+const setup = ({ data = {}, config = {}, params = {}, events = {} } = {}) => ({
+  user: userEvent.setup(),
+  ...render(
     <MockJetsContextProvider config={config} params={{ ...paramsData(params) }} events={events}>
       <JetsTooltipGlobal data={{ ...activeTooltipData(data) }} />
     </MockJetsContextProvider>
-  );
-};
+  ),
+});
 
 describe('JetsTooltipGlobal', () => {
   describe('when a tooltip is rendered', () => {
@@ -134,51 +134,46 @@ describe('JetsTooltipGlobal', () => {
   });
 
   describe('when a button in the tooltip is pressed', () => {
-    it('should fire onSeatSelect when "Select" is pressed', () => {
+    it('should fire onSeatSelect when "Select" is pressed', async () => {
       const onSeatSelect = jest.fn();
+      const { user } = setup({ events: { onSeatSelect } });
 
-      setup({ events: { onSeatSelect } });
-
-      userEvent.click(screen.getByText(/Select/));
+      await user.click(screen.getByText(/Select/));
       expect(onSeatSelect).toHaveBeenCalledTimes(1);
     });
 
-    it('should not fire onSeatSelect when seat selection is disabled and "Select" is pressed', () => {
+    it('should not fire onSeatSelect when seat selection is disabled and "Select" is pressed', async () => {
       const onSeatSelect = jest.fn();
+      const { user } = setup({ events: { isSeatSelectDisabled: () => true, onSeatSelect } });
 
-      setup({ events: { isSeatSelectDisabled: () => true, onSeatSelect } });
-
-      userEvent.click(screen.getByText(/Select/));
+      await user.click(screen.getByText(/Select/));
       expect(onSeatSelect).not.toHaveBeenCalled();
     });
 
-    it('should fire onSeatUnselect when "Unselect" is pressed', () => {
+    it('should fire onSeatUnselect when "Unselect" is pressed', async () => {
       const onSeatUnselect = jest.fn();
+      const { user } = setup({ data: { passenger: { passengerLabel: 'Dave Smith' } }, events: { onSeatUnselect } });
 
-      setup({ data: { passenger: { passengerLabel: 'Dave Smith' } }, events: { onSeatUnselect } });
-
-      userEvent.click(screen.getByText(/Unselect/));
+      await user.click(screen.getByText(/Unselect/));
       expect(onSeatUnselect).toHaveBeenCalledTimes(1);
     });
 
-    it('should not fire onSeatUnselect when the passenger is readOnly and "Unselect" is pressed', () => {
+    it('should not fire onSeatUnselect when the passenger is readOnly and "Unselect" is pressed', async () => {
       const onSeatUnselect = jest.fn();
-
-      setup({
+      const { user } = setup({
         data: { passenger: { passengerLabel: 'Dave Smith', readOnly: true } },
         events: { isSeatSelectDisabled: () => true, onSeatUnselect },
       });
 
-      userEvent.click(screen.getByText(/Unselect/));
+      await user.click(screen.getByText(/Unselect/));
       expect(onSeatUnselect).not.toHaveBeenCalled();
     });
 
-    it('should fire onTooltipClose when "Cancel" is pressed', () => {
+    it('should fire onTooltipClose when "Cancel" is pressed', async () => {
       const onTooltipClose = jest.fn();
+      const { user } = setup({ events: { onTooltipClose } });
 
-      setup({ events: { onTooltipClose } });
-
-      userEvent.click(screen.getByText(/Cancel/));
+      await user.click(screen.getByText(/Cancel/));
       expect(onTooltipClose).toHaveBeenCalledTimes(1);
     });
   });
